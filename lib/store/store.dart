6d171f7370f076f45d5400312776db0fd4b1d774
rquanx <runquant@gmail.com>
2025-04-23 01:23:28 +0800

    refactor(store): 移除未使用的ComplexState抽象类
    移除未使用的ComplexState抽象类以简化代码结构，提高代码可维护性

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -1,9 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-abstract class ComplexState {
-  ComplexState copyWith({int? count});
-}
-
 ({T state, dynamic Function(T Function(T)) update}) Function(WidgetRef)
 useStore<T>(StateProvider<T> provider) {
   ({T state, Function(T Function(T)) update}) x(WidgetRef ref) {
