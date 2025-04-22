b701eb765ff912dbcc7812b51ac4351365385441
rquanx <runquant@gmail.com>
2025-04-23 00:13:32 +0800

    feat: fquery example

--- lib/components/counter.dart
+++ lib/components/counter.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/hooks/example.dart';
+import 'package:flutter_application_1/services/post.dart';
+import 'package:fquery/fquery.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_application_1/store/counter.dart';
 
 class Counter extends HookConsumerWidget {
   const Counter({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final posts = useQuery(['posts'], getPosts);
+
     final funHook = useLoggedState(2);
     final classHook = useTimeAlive();
     final x = ref.watch(counterProvider);
+
     return Column(
       children: [
         Text(x.toString()),
@@ -22,6 +28,27 @@ class Counter extends HookConsumerWidget {
         ElevatedButton(
           onPressed: () => {funHook.value = (funHook.value ?? 0) + 1},
           child: const Text('update hooks'),
+        ),
+        Builder(
+          builder: (context) {
+            if (posts.isLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (posts.isError) {
+              return Center(child: Text(posts.error!.toString()));
+            }
+
+            return ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: (posts.data?.length ?? 0) > 0 ? 10 : 0,
+              itemBuilder: (context, index) {
+                final post = posts.data![index];
+                return ListTile(title: Text(post.title));
+              },
+            );
+          },
         ),
       ],
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:fquery/fquery.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import './router.dart';
 
+final queryClient = QueryClient(
+  defaultQueryOptions: DefaultQueryOptions(
+    cacheDuration: Duration(minutes: 20),
+    refetchInterval: Duration(minutes: 5),
+    refetchOnMount: RefetchOnMount.always,
+    staleDuration: Duration(minutes: 3),
+  ),
+);
+
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+    ProviderScope(
+      child: QueryClientProvider(
+        queryClient: queryClient,
+        child: const ProviderScope(child: MyApp()),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
