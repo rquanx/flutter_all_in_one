d941b06d543912a7704dd5285f70e2a55e9bfd3a
rquanx <runquant@gmail.com>
2025-04-23 01:27:12 +0800

    refactor(store): 添加 raw 方法以获取 StateController
    为了增强 store 的功能，允许直接获取 StateController 实例，以便在需要时进行更精细的状态管理。

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -1,13 +1,26 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-({T state, dynamic Function(T Function(T)) update}) Function(WidgetRef)
+({
+  StateController<T> Function() raw,
+  T state,
+  dynamic Function(T Function(T)) update,
+})
+Function(WidgetRef)
 useStore<T>(StateProvider<T> provider) {
-  ({T state, Function(T Function(T)) update}) x(WidgetRef ref) {
+  ({
+    StateController<T> Function() raw,
+    T state,
+    dynamic Function(T Function(T)) update,
+  })
+  x(WidgetRef ref) {
     final state = ref.watch(provider);
     return (
       state: state,
       update: (T Function(T state) cb) {
         ref.read(provider.notifier).update(cb);
+      },
+      raw: () {
+        return ref.read(provider.notifier);
       },
     );
   }
