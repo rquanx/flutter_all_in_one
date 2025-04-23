323bc5b4a7e39de2d1418209c6ba08c9c785845d
rquanx <runquant@gmail.com>
2025-04-23 17:08:33 +0800

    refactor(store): 简化 useStore 函数实现

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -1,28 +1,19 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-({
-  StateController<T> Function() raw,
-  T state,
-  dynamic Function(T Function(T)) update,
-})
-Function(WidgetRef)
-useStore<T>(StateProvider<T> provider) {
-  ({
-    StateController<T> Function() raw,
-    T state,
-    dynamic Function(T Function(T)) update,
-  })
-  x(WidgetRef ref) {
+typedef Store<T> =
+    ({
+      StateController<T> Function() raw,
+      T state,
+      void Function(T Function(T)) update,
+    });
+
+Store<T> Function(WidgetRef) useStore<T>(StateProvider<T> provider) {
+  return (WidgetRef ref) {
     final state = ref.watch(provider);
     return (
+      raw: () => ref.read(provider.notifier),
       state: state,
-      update: (T Function(T state) cb) {
-        ref.read(provider.notifier).update(cb);
-      },
-      raw: () {
-        return ref.read(provider.notifier);
-      },
+      update: (cb) => ref.read(provider.notifier).update(cb),
     );
-  }
-  return x;
+  };
 }
